490dd316c36a54dad71428074f2a1b717fc34a0d
meta-nikhil-srivastava <nikhil.srivastava@metacube.com>
2016-08-19 11:32:50 +0530

    Update assignment1.sql

--- assignment1.sql
+++ assignment1.sql
@@ -5,7 +5,22 @@ INSERT INTO members(member_nm,addressline1,adddessline2,category) VALUES ("John 
 
 SELECT member_nm FROM members WHERE category IN ( SELECT DISTINCT category FROM members WHERE member_nm="John Snow");
 
+
 /*Q2*/
+
+SELECT 
+bi.issue_dt AS Issue_Date, t.title_nm AS Title,
+m.member_nm AS Member, bi.due_dt AS Due_Date FROM book_issue bi
+INNER JOIN members m ON bi.member_id = m.member_id
+INNER JOIN books b ON bi.accession_no = b.accession_no
+INNER JOIN titles t ON b.title_id = t.title_id
+WHERE (bi.issue_dt, bi.accession_no, bi.member_id) IN
+(SELECT br.issue_dt, br.accession_no, br.member_id FROM book_return br
+WHERE TIMESTAMPDIFF(DAY, bi.due_dt, br.return_dt) > 0);
+
+
+/*Q3*/
+
 
 SELECT t.title_nm AS Title, m.member_nm AS Member,bi.issue_dt AS Issue_Date,bi.due_dt AS Due_Date FROM book_issue bi
 INNER JOIN members m ON bi.member_id = m.member_id
@@ -18,17 +33,7 @@ SELECT br.issue_dt, br.accession_no, br.member_id FROM book_return br
 );
 
 
-/*Q3*/
 
-SELECT 
-bi.issue_dt AS Issue_Date, t.title_nm AS Title,
-m.member_nm AS Member, bi.due_dt AS Due_Date FROM book_issue bi
-INNER JOIN members m ON bi.member_id = m.member_id
-INNER JOIN books b ON bi.accession_no = b.accession_no
-INNER JOIN titles t ON b.title_id = t.title_id
-WHERE (bi.issue_dt, bi.accession_no, bi.member_id) IN
-(SELECT br.issue_dt, br.accession_no, br.member_id FROM book_return br
-WHERE TIMESTAMPDIFF(DAY, bi.due_dt, br.return_dt) > 0);
 
 /*Q4*/
 
