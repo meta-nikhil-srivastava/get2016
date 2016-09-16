78d4ce259e1bef90934ea2a9d568632d128b20d2
meta-nikhil-srivastava <nikhil.srivastava@metacube.com>
2016-09-16 16:26:15 +0530

    Update table.js

--- assignment4/resources/js/table.js
+++ assignment4/resources/js/table.js
@@ -1,9 +1,14 @@
-//color table with different background color for alternate rows on load of the document
 $(document).ready(function () {
-    $("#button").click(function () {       
-        $("tr:even").css("backgroundColor", "pink"); //color even rows
-        $("tr:odd").css("backgroundColor", "orange"); 
-        //color odd rows
-                       
+    $("#button").click(function () {
+        if ($("#button").text() === "Change") {
+            $("tr:even").css("backgroundColor", "pink");
+            $("tr:odd").css("backgroundColor", "orange");
+            //color odd rows
+            $("#button").text("Revert");
+        }
+        else {
+            $("tr").css("backgroundColor", "white");
+            $("#button").text("Change");
+        }
     });
 });
